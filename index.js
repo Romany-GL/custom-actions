82a1369544e3037deabc8a3c62fe7d447c21c8a6
Romany-GL <romany.sefen@globallogic.com>
2024-04-22 12:53:21 +0100

    add outputs and logs

--- index.js
+++ index.js
@@ -8,8 +8,24 @@ try {
   const time = new Date().toLocaleTimeString();
   console.log(`The time is ${time}`);
 
+  // Access the event payload
   const payload = JSON.stringify(github.context.payload, null, 2);
+
+  core.setOutput('time', time);
+
+  // Set the values as environment variables, so they can be used in subsequent steps
+  // echo $GREETING_NAME in the workflow will output the value of the nameToGreet input
+  // echo $GREETING_TIME in the workflow will output the value of the time variable
+  core.exportVariable('GREETING_NAME', nameToGreet);
+  core.exportVariable('GREETING_TIME', time);
+
+  core.startGroup('Logging the event payload');
   console.log(`The event payload: ${payload}`);
+  core.endGroup();
+
+  core.info('This is a log message');
+  core.warning('This is a warning message');
+  core.error('This is an error message');
 } catch (error) {
   core.setFailed(error.message);
 }
